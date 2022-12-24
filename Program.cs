fa153f7ec4a649f54097b302092a94c72e279b50
Emiliano23atr <joaovitoremiliano5@gmail.com>
2022-12-23 22:01:40 -0300

    Iniciando o projeto

--- Program.cs
+++ Program.cs
@@ -1,8 +1,46 @@
-﻿namespace TextEditor;
+﻿using System;
+namespace TextEditor;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        menu();
     }
+
+    static void menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que você deseja fazer?");
+        Console.WriteLine("1 - Abrir arquivo");
+        Console.WriteLine("2 - Criar novo arquivo");
+        Console.WriteLine("0 - Sair");
+        short option = short.Parse(Console.ReadLine());
+
+        switch(option)
+        {
+            case 0: 
+                System.Environment.Exit(0);
+                break;
+            case 1:
+                Abrir();
+                break;
+            case 2:
+                Editar();
+                break;
+            default:
+                menu();
+                break;    
+        }
+    }
+
+    static void Abrir()
+    {
+
+    }
+
+    static void Editar()
+    {
+
+    }
+
 }
